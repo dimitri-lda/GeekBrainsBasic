f016134c2ea59f1d9e2040d8ada1ef85cf795252
Dzmitry Lukashuk <dmitry.lukashuk@sobrado.ch>
2023-03-24 13:10:46 +0100

    Final - added test arrays

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,8 @@
 public class Solution {
     public static void main(String[] args) {
-
+        String[] array1 = new String[]{"hello","2","world",":-)"};
+        String[] array2 = new String[]{"1234","1567","-2","computer science"};
+        String[] array3 = new String[]{"Belarus","Denmark","Brest"};
     }
 
     private static boolean isStringShortEnough(String str, int requiredLength) {
