e6876a948f7d86f1a35ed26e88a27cccf53ccda7
Dzmitry Lukashuk <dmitry.lukashuk@sobrado.ch>
2023-03-24 13:34:56 +0100

    Final - added empty method and finished main method

--- src/Solution.java
+++ src/Solution.java
@@ -1,8 +1,20 @@
 public class Solution {
     public static void main(String[] args) {
+        int requiredLength = 3;
         String[] array1 = new String[]{"hello","2","world",":-)"};
         String[] array2 = new String[]{"1234","1567","-2","computer science"};
         String[] array3 = new String[]{"Belarus","Denmark","Brest"};
+        String[] fixedArray1 = getArrayOfCorrectLengthElements(array1, requiredLength);
+        String[] fixedArray2 = getArrayOfCorrectLengthElements(array2, requiredLength);
+        String[] fixedArray3 = getArrayOfCorrectLengthElements(array3, requiredLength);
+        printArray(fixedArray1);
+        printArray(fixedArray2);
+        printArray(fixedArray3);
+    }
+
+    private static String[] getArrayOfCorrectLengthElements(String[] array, int requiredLength) {
+        String[] fixedArray = new String[]{};
+        return fixedArray;
     }
 
     private static boolean isStringShortEnough(String str, int requiredLength) {
