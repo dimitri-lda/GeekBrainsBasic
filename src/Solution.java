b39875d632344ba1b7b915581a12d935792e4ceb
Dzmitry Lukashuk <dmitry.lukashuk@sobrado.ch>
2023-03-24 13:00:41 +0100

    Final - added isStringShortEnough method

--- src/Solution.java
+++ src/Solution.java
@@ -2,4 +2,8 @@ public class Solution {
     public static void main(String[] args) {
 
     }
+
+    private static boolean isStringShortEnough(String str, int requiredLength) {
+        return str.length() <= requiredLength;
+    }
 }
