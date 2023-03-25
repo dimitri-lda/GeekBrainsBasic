8aaf081c0a27a1051adecb5200f6be2409725987
Dzmitry Lukashuk <dmitry.lukashuk@sobrado.ch>
2023-03-25 18:06:52 +0100

    Final - added diagram and description to readme

--- src/Solution.java
+++ src/Solution.java
@@ -1,38 +1,38 @@
 public class Solution {
     public static void main(String[] args) {
-        int requiredLength = 3;
+        int requiredStringLength = 3;
         String[] array1 = new String[]{"hello","2","world",":-)"};
         String[] array2 = new String[]{"1234","1567","-2","computer science"};
         String[] array3 = new String[]{"Belarus","Denmark","Brest"};
-        String[] fixedArray1 = getArrayOfCorrectLengthElements(array1, requiredLength);
-        String[] fixedArray2 = getArrayOfCorrectLengthElements(array2, requiredLength);
-        String[] fixedArray3 = getArrayOfCorrectLengthElements(array3, requiredLength);
-        printArray(fixedArray1);
-        printArray(fixedArray2);
-        printArray(fixedArray3);
+        String[] resultArray1 = getArrayOfCorrectLengthElements(array1, requiredStringLength);
+        String[] resultArray2 = getArrayOfCorrectLengthElements(array2, requiredStringLength);
+        String[] resultArray3 = getArrayOfCorrectLengthElements(array3, requiredStringLength);
+        printArray(resultArray1);
+        printArray(resultArray2);
+        printArray(resultArray3);
         System.out.println();
 
         // Additional solution with recursion and without getArraySize method
-        int startFixedArraySize = 0;
-        fixedArray1 = getArrayOfCorrectLengthElementsRecursion(array1, startFixedArraySize, requiredLength);
-        fixedArray2 = getArrayOfCorrectLengthElementsRecursion(array2, startFixedArraySize, requiredLength);
-        fixedArray3 = getArrayOfCorrectLengthElementsRecursion(array3, startFixedArraySize, requiredLength);
-        printArray(fixedArray1);
-        printArray(fixedArray2);
-        printArray(fixedArray3);
+        int startResultArraySize = 0;
+        resultArray1 = getArrayOfCorrectLengthElementsRecursion(array1, startResultArraySize, requiredStringLength);
+        resultArray2 = getArrayOfCorrectLengthElementsRecursion(array2, startResultArraySize, requiredStringLength);
+        resultArray3 = getArrayOfCorrectLengthElementsRecursion(array3, startResultArraySize, requiredStringLength);
+        printArray(resultArray1);
+        printArray(resultArray2);
+        printArray(resultArray3);
     }
 
     private static String[] getArrayOfCorrectLengthElements(String[] array, int requiredLength) {
-        int fixedArraySize = getArraySizeOfCorrectLengthElements(array, requiredLength);
-        String[] fixedArray = new String[fixedArraySize];
+        int resultArraySize = getArraySizeOfCorrectLengthElements(array, requiredLength);
+        String[] resultArray = new String[resultArraySize];
         int index = 0;
         for (String str : array) {
             if (isStringShortEnough(str, requiredLength)) {
-                fixedArray[index] = str;
+                resultArray[index] = str;
                 index++;
             }
         }
-        return fixedArray;
+        return resultArray;
     }
 
     private static int getArraySizeOfCorrectLengthElements(String[] array,  int requiredLength) {
@@ -58,25 +58,25 @@ public class Solution {
 
     private static String[] getArrayOfCorrectLengthElementsRecursion(
         String[] array,
-        int fixedArraySize,
+        int resultArraySize,
         int requiredLength
     ) {
-        String[] fixedArray = new String[fixedArraySize];
+        String[] resultArray = new String[resultArraySize];
         int index = 0;
         for (String str : array) {
             if (isStringShortEnough(str, requiredLength)) {
-                if (index + 1 > fixedArraySize) {
-                    fixedArray = getArrayOfCorrectLengthElementsRecursion(
+                if (index + 1 > resultArraySize) {
+                    resultArray = getArrayOfCorrectLengthElementsRecursion(
                             array,
-                            fixedArraySize + 1,
+                            resultArraySize + 1,
                             requiredLength
                     );
                 } else {
-                    fixedArray[index] = str;
+                    resultArray[index] = str;
                     index++;
                 }
             }
         }
-        return fixedArray;
+        return resultArray;
     }
 }
